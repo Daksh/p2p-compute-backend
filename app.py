35316e0e48d50bf1b7463ee02e78782af04aaea4
Daksh Shah <daksh17336@iiitd.ac.in>
2022-02-27 00:22:39 +0000

    dont remember

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session, copy_current_request_context
-from flask_socketio import SocketIO, emit, disconnect
+from flask_socketio import SocketIO, emit, disconnect, join_room, leave_room
+from flask import request
 
 async_mode = None
 app = Flask(__name__)
@@ -37,6 +38,32 @@ def disconnect_request():
          {'data': 'Disconnected!', 'count': session['receive_count']},
          callback=can_disconnect)
 
+@socket_.on('join', namespace='/test')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    print("request.sid is ",request.sid)
+    print('join got this (username, room):', username, room)
+    join_room(room)
+    # emit('my_response', {'data':username+' has joined the room.'}, broadcast=True)
+    emit('my_response', {'[sent from server] data':username+' has joined the room.'}, to='myroomname')
+    # print("DONE SENDING")
+
+# @socket_.on('leave', namespace='/test')
+# def on_leave(data):
+#     username = data['username']
+#     room = data['room']
+#     leave_room(room)
+#     emit('room_response', {'data':username+' has left the room.'}, to=room, broadcast=True)
+
+@socket_.on('room_response', namespace='/test')
+def on_myroom(data):
+    print("LOOOK OUT FOR THIS i got this message", request.sid)
+    emit('my_response', data)
+
+@socket_.on('my_response_some', namespace='/test')
+def log(data):
+    print("LOG:",data)
 
 if __name__ == '__main__':
     socket_.run(app, debug=True, host='0.0.0.0', port='5255')
