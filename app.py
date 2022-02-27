337e10489882047120572c5fb48af3235361e8f0
Daksh Shah <daksh17336@iiitd.ac.in>
2022-02-27 00:44:56 +0000

    Set up the central server

--- app.py
+++ app.py
@@ -1,42 +1,43 @@
-from flask import Flask, render_template, session, copy_current_request_context
+from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit, disconnect
+import random
 
 async_mode = None
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socket_ = SocketIO(app, async_mode=async_mode)
+available_compute_sockets = []
 
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socket_.async_mode)
 
+@socket_.on('register_compute')
+def register_compute():
+    available_compute_sockets.append(request.sid)
+    print(f"{request.sid} registered")
+    print(f"[after append] available_compute_sockets: {available_compute_sockets}")
+    
+# Global host server (this guy) receives a file from local server
+@socket_.on('send_file_to_global')
+def receive_file(file):
+    chosen_socket = random.choice(available_compute_sockets)
+    emit('send_file_to_local', file, to=chosen_socket)
 
-@socket_.on('my_event', namespace='/test')
-def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
+@socket_.on('unregister_compute')
+def unregsiter_compute():
+    if request.sid in available_compute_sockets:
+        available_compute_sockets.remove(request.sid)
+    print(f"{request.sid} unregistered")
+    print(f"[after remove] available_compute_sockets: {available_compute_sockets}")
 
-
-@socket_.on('my_broadcast_event', namespace='/test')
-def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
-
-
-@socket_.on('disconnect_request', namespace='/test')
-def disconnect_request():
-    @copy_current_request_context
-    def can_disconnect():
-        disconnect()
-
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']},
-         callback=can_disconnect)
+@socket_.on('disconnect')
+def disconnect():
+    if request.sid in available_compute_sockets:
+        available_compute_sockets.remove(request.sid)
+    print(f"{request.sid} disconnected, so I will unregister")
+    print(f"[after remove] available_compute_sockets: {available_compute_sockets}")
 
 
 if __name__ == '__main__':
-    socket_.run(app, debug=True, host='0.0.0.0', port='5255')
+    socket_.run(app, debug=True, host='0.0.0.0', port='5252')
